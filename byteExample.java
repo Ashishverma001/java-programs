33e5d13d886795cd3dcf0f5bd772cf7f22d04d34
Ashishverma001 <111993809+Ashishverma001@users.noreply.github.com>
2023-12-20 17:19:16 +0530

    datatype
    done

---
+++ byteExample.java
@@ -0,0 +1,12 @@
+// PREMETIVE DATATYPE 
+
+// BYTE 
+public class byteExample {
+
+    public static void main(String[] args) {
+        byte b=20;
+        System.out.println(b);
+    }
+ 
+}
+
---
+++ testingshort.java
@@ -0,0 +1,11 @@
+public class testingshort {
+
+    public static void main(String[] args){
+		short a=-32768;
+		short b=32767;
+		System.out.println(""+a);
+		System.out.println(""+b);
+	}
+}
+
+
